30d9e4db8b44b93b47fb326b300dae08a2f5b25c
Erjoh <erjik1902@gmail.com>
2023-07-26 14:10:31 +0600

    novyione

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const input = document.querySelector('#input')
 const start = document.querySelector('#start')
 const res = document.querySelector('#res')
+const resran = document.querySelector('#resoftandom')
 
 const handleCheckAns = () => {
     const randonNum = Math.floor(Math.random() * 10) + 1;
     const innerInput = parseInt(input.value);
     if (innerInput >= 1 && innerInput <= 10) {
-        if (innerInput === 6){
-            return 'Ty vyigral'
+        if (innerInput === randonNum){
+            return `Ty vyigral, randomnym chislom bylo ${randonNum}`
         } else {
-            return 'Davai esche raz'
+            return `Davai esche raz, randomnym chislom bylo ${randonNum}`
         }
     } else {
-        return 'Chislo dolzhno bytь bolьshe 0 i menьshe 10'
+        return 'Chislo dolzhno bytь bolьshe 0 i menьshe ili ravnym10'
     }
 }
 
